d90bdcbe133e1d86afe24c90b7d36af7971c2c45
Joshua Crocker <47287266+Jasta1234@users.noreply.github.com>
2024-07-08 09:22:50 -0230

    Finished Div A-C

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const $ = selector => document.querySelector(selector);
 
+    // Array to hold all box cars
+    const boxCarArray = [];
+
     // Get all the divs required
     const divA = $("#divA");
     const divB = $("#divB");
@@ -18,6 +21,31 @@ document.addEventListener("DOMContentLoaded", () => {
     const radioE = $("#divERadio");
     const radioF = $("#divFRadio");
     const radioG = $("#divGRadio");
+
+    // Get all the inputs for div B (Create box car)
+    const boxCarIDInput = $("#boxcarID");
+    const tareWeightInput = $("#tareWeight");
+    const maxGrossWeightInput = $("#maxGrossWeight");
+    const cargoWeightInput = $("#cargoWeight");
+    const grossWeightInput = $("#grossWeight");
+
+    // Get all the buttons required for div B (Create box car)
+    const processBoxCarBtn = $("#processBoxCar");
+    const divBResetFormBtn = $("#divBResetForm");
+    const divBReturnToMainPageBtn = $("#divBReturnMainPage")
+
+    // Get all the spans required for div B (Create box car)
+    const tareWeightSpan = $("#tareWeightSpan");
+    const maxGrossWeightSpan = $("#maxGrossWeightSpan");
+
+    // Get the table for div C (Display All Box Cars)
+    const configuredBoxCarTable = $("#configuredBoxCarTable");
+    const totalCargoWeightIntValue = $("#totalCargoWeightIntValue");
+
+    // Get all the buttons required for div C (Display All Box Cars)
+    // NBOT COMPLETED 
+    const returnToCreateBoxCar = $("#returnToCreateBoxCar");
+    const divCReturnToMainPageBtn = $("#divCReturnMainPage");
 
     /* 
         Function that takes in a div, and hides all other divs except for the ones required.
@@ -41,10 +69,102 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
+    /*
+        Function which passes in the Box Car Array.
+        Checks if certain conditions are met, if they are, it will append the info to an array, and then to the master array.
+        It will then display this information on the Configured Boxcar table.
+    */
+    const processBoxCar = (boxCarArray) => {
+        // Get the value of the Div B, create box car inputs.
+        let boxCarIDVal = boxCarIDInput.value;
+        let tareWeightVal = parseInt(tareWeightInput.value);
+        let maxGrossWeightVal = parseInt(maxGrossWeightInput.value);
+        let cargoWeightVal = parseInt(cargoWeightInput.value);
+        let grossWeightVal = parseInt(grossWeightInput.value);
+
+        // Set the value of grossWeight.
+        grossWeightVal = tareWeightVal + cargoWeightVal;
+
+        // If statements to display errors.
+        if (tareWeightVal < 0 || tareWeightVal > 20000) {
+            tareWeightSpan.textContent = "TARE Weight must be in the range of 0 to 20000";
+        } else if (maxGrossWeightVal < tareWeightVal || maxGrossWeightVal > 200000) {
+            maxGrossWeightSpan.textContent = "Must be greater than TARE and must be in range of 0 to 200000";
+        // Else statement to proceed with the function.
+        } else {
+            const currentBoxCar = {
+                boxCarID: boxCarIDVal,
+                tareWeight: tareWeightVal,
+                maxGrossWeight: maxGrossWeightVal,
+                cargoWeight: cargoWeightVal,
+                grossWeight: grossWeightVal
+            };
+            boxCarArray.push(currentBoxCar);
+            displayConfiguredBoxCars(boxCarArray);
+        }
+    };
+
+    /*
+        Function which takes in boxCarArray.
+        Checks to see if a body exists in the table, and if so, deletes it.
+        loops through the boxCarArray, gets each object, and then displays the info in the table.
+
+    */
+    const displayConfiguredBoxCars = (boxCarArray) => {
+        // Check if tbody exists and remove it
+        let oldTbody = configuredBoxCarTable.querySelector('tbody');
+        if (oldTbody) {
+            configuredBoxCarTable.removeChild(oldTbody);
+            totalCargoWeightIntValue.textContent = "0";
+        }
+    
+        let tableBody = document.createElement('tbody');
+        let totalCargoWeight = 0;
+    
+        boxCarArray.forEach(boxCar => {
+            let tableRow = document.createElement('tr');
+            for (let key in boxCar) {
+                let tableCell = document.createElement('td');
+                tableCell.textContent = boxCar[key];
+                tableRow.append(tableCell);
+    
+                // Check if the key is 'cargoWeight' and add to totalCargoWeight
+                if (key === 'cargoWeight') {
+                    totalCargoWeight += parseFloat(boxCar[key]);
+                }
+            }
+            tableBody.append(tableRow);
+        });
+    
+        configuredBoxCarTable.append(tableBody);
+        totalCargoWeightIntValue.textContent = totalCargoWeight;
+    };
+    
+
+    /*
+        Function which passes in "inputsToReset" and "inputDefaultValue"
+        This is a master function to reset forms.
+        {inputsToReset} is a list of inputs which need to be reset.
+        {inputDefaultValue} is a list of the default values for the inputs that are being reset.
+    */
+    const resetForm = (inputsToReset, inputDefaultValue) => {
+        inputsToReset.forEach(input => {
+            inputDefaultValue.forEach(value => {
+                input.value = value;
+            })
+        });
+    };
+
     // Attach event listeners to radio buttons
     radioB.addEventListener("click", () => changeDiv(divB));
     radioD.addEventListener("click", () => changeDiv(divD));
     radioE.addEventListener("click", () => changeDiv(divE));
     radioF.addEventListener("click", () => changeDiv(divF));
     radioG.addEventListener("click", () => changeDiv(divG));
+
+    // Attach event listeners to buttons
+    processBoxCarBtn.addEventListener("click", () => processBoxCar(boxCarArray));
+    divBResetFormBtn.addEventListener("click", () => resetForm([boxCarIDInput, tareWeightInput, maxGrossWeightInput], [" ", 0, 0]));
+    divBReturnToMainPageBtn.addEventListener("click", () => changeDiv(divA));
+    divCReturnToMainPageBtn.addEventListener("click", () => changeDiv(divA));
 });
